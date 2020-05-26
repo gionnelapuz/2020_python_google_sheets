76d07e7c3ce0a0a6c1316560e9e3554154bdee78
Gionne lapuz <gionne.lapuz@gmail.com>
2020-05-26 15:45:08 +0400

    Updated and Optimized File List and Counter

--- listFiles.py
+++ listFiles.py
@@ -1,5 +1,4 @@
 import httplib2
-import os
 from apiclient import discovery
 from google.oauth2 import service_account
 
@@ -11,22 +10,22 @@ import shutil
 
 scopes = ["https://www.googleapis.com/auth/drive", "https://www.googleapis.com/auth/drive.file", "https://www.googleapis.com/auth/spreadsheets"]
 secret_file = 'credentials.json'
-spreadsheet_id = '1j08PSHvlKnIfmMsF_yyL3SqIP9JfZpW3aWcwXSNvhO4'
+spreadsheet_id = '1vJ1S909ivuaiHyAJEUAUji3LdJ9tao6V0Ku4DQFXry8'
 range_name = 'Sheet1!A2:A'
 credentials = service_account.Credentials.from_service_account_file(secret_file, scopes=scopes)
 service = discovery.build('sheets', 'v4', credentials=credentials)
 
-folder_path = '/mnt/d/Movies'
+folder_path = '/mnt/d/Series'
+
 video_extensions = ['.mp4', '.mkv', '.m4v', '.avi']
 videos_insert_array = []
 
 
-def getFromSheet():
+def getDataFromSheet():
     result_input = service.spreadsheets().values().get(
         spreadsheetId=spreadsheet_id, 
         range=range_name
     ).execute()
-
     if result_input.get('values'):
         getFolderFiles(list(chain.from_iterable(result_input.get('values'))))
     else:
@@ -45,8 +44,6 @@ def insertToSheet():
     ).execute()
 
 
-
-
 def splitFileData(file):
     return list(os.path.splitext(file))
 
@@ -62,24 +59,20 @@ def getFolderFiles(sheet_files):
     else:
         video_files = os.listdir(folder_path)
 
-
     if video_files:
         for main_directory_files in video_files:
             main_directory_file_path = folder_path + '/' + main_directory_files
-        
             if os.path.isfile(main_directory_file_path):
                 videos_insert_array.append([main_directory_files, 'Movie'])
             else:
-                for subdir, sub_directories, sub_directory_files in os.walk(main_directory_file_path):
-                    video_count = len([sub_directory_file for sub_directory_file in sub_directory_files if splitFileData(sub_directory_file)[1] in video_extensions])
-                    
-                    if(video_count > 2):
-                        videos_insert_array.append([main_directory_files, 'Series'])
-                    else:
-                        videos_insert_array.append([main_directory_files, 'Movie'])
-                    break
+                files_from_folders = [sub_directory_files for subdir, sub_directories, sub_directory_files in os.walk(main_directory_file_path)]
+                without_hidden_files_count = len([without_hidden_files for without_hidden_files in list(chain.from_iterable(files_from_folders)) if splitFileData(without_hidden_files)[1] in video_extensions and not without_hidden_files.startswith('.')])
+
+                if(without_hidden_files_count == 1):
+                    videos_insert_array.append([main_directory_files, 'Movie'])
+                elif(without_hidden_files_count > 1):
+                    videos_insert_array.append([main_directory_files, 'Series'])
         insertToSheet()
-                    
 
 
-getFromSheet()
+getDataFromSheet()
